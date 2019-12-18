e4ed21f5a16eed5e30a7b9af7f0beaa81f422b08
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2019-12-18 13:40:21 +0200

    telegram system properties

--- src/telefunken/telegram.clj
+++ src/telefunken/telegram.clj
@@ -3,18 +3,18 @@
             [lang-utils.async :refer [with-async]]
             [clojure.string :as str]))
 
-(def self-test #(client/get (str "https://api.telegram.org/bot" (System/getProperty "telefunken.bot.token") "/getMe")
+(def self-test #(client/get (str "https://api.telegram.org/bot" (System/getProperty "telegram.bot.token") "/getMe")
                             {:throw-exceptions false
                              :as :auto}))
 
-(def get-updates #(client/get (str "https://api.telegram.org/bot" (System/getProperty "telefunken.bot.token") "/getUpdates")
+(def get-updates #(client/get (str "https://api.telegram.org/bot" (System/getProperty "telegram.bot.token") "/getUpdates")
                               {:throw-exceptions false
                                :as :auto}))
 
 (defn send-message [text]
-  (client/post (str "https://api.telegram.org/bot" (System/getProperty "telefunken.bot.token") "/sendMessage")
+  (client/post (str "https://api.telegram.org/bot" (System/getProperty "telegram.bot.token") "/sendMessage")
                {:throw-exceptions false
-                :form-params {:chat_id (Integer. ^String (System/getProperty "telefunken.chat.id"))
+                :form-params {:chat_id (Integer. ^String (System/getProperty "telegram.chat.id"))
                               :text text
                               :parse_mode "Markdown"}
                 :as :json}))
