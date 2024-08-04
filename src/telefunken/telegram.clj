bedb3e37c76fa093e5739adf142ef885b8984cc5
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2024-08-04 18:22:57 +0300

    async-send-message
    - Fix: Telegram throws error on "foo_" in Markdown mode because it is not valid
    - Send non-formatted text
    - Additional methods for formatted text

--- src/telefunken/telegram.clj
+++ src/telefunken/telegram.clj
@@ -11,15 +11,25 @@
                               {:throw-exceptions false
                                :as :auto}))
 
-(defn send-message [text]
-  (client/post (str "https://api.telegram.org/bot" (System/getProperty "telegram.bot.token") "/sendMessage")
+(defn send-message
+  ([text]
+   (send-message text {:parse-mode ""}))
+  ([text {:keys [parse-mode]}]
+   {:pre [(some #{parse-mode} ["MarkdownV2" "Markdown" "HTML" ""])]}
+   (client/post (str "https://api.telegram.org/bot" (System/getProperty "telegram.bot.token") "/sendMessage")
                {:throw-exceptions false
                 :form-params {:chat_id (Integer. ^String (System/getProperty "telegram.chat.id"))
                               :text text
-                              :parse_mode "Markdown"}
-                :as :json}))
+                              :parse_mode parse-mode}
+                :as :json})))
 
 (defn async-send-message [& text]
   (with-async (send-message (str/join " " text))))
+
+(defn async-send-html [& text]
+  (with-async (send-message (str/join " " text) {:parse-mode "HTML"})))
+
+(defn async-send-markdown [& text]
+  (with-async (send-message (str/join " " text) {:parse-mode "MarkdownV2"})))
 
 (defn pulse [interval] (async-send-message (str "*Pulse* _" interval "_")))
